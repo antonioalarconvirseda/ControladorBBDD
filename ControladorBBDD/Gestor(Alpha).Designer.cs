ac3ecc858c48dc25b9ae777d8e4822cf10ff4b59
josegygi <43671402+josegygi@users.noreply.github.com>
2019-10-14 19:54:40 +0200

    Primera sangre.

---
+++ ControladorBBDD/Gestor(Alpha).Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ControladorBBDD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Query";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tablas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cerrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(32, 183);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(343, 144);
+            this.textBox2.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(393, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(404, 332);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(32, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(255, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Query";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 368);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gestor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.DataGridView dataGridView1 = new System.Windows.Forms.DataGridView();
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ControladorBBDD/Gestor(Alpha).cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ControladorBBDD
+{
+    public partial class Form1 : Form
+    {
+
+        string connString = "";
+        MySqlConnection sqlConn = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 || textBox2.Text.Length > 0)
+            {
+                connString = "datasource=127.0.0.1;port=3306;username=root;password=;database=" + textBox1.Text + ";";
+
+                using (sqlConn = new MySqlConnection(connString))
+                {
+                    try
+                    {
+                        sqlConn.Open();
+                        var result = MySqlCollectionQuery(sqlConn, "show tables");
+                        comboBox1.Items.Clear();
+                        foreach (string nombre in result)
+                        {
+                            comboBox1.Items.Add(nombre);
+                        }
+                        string sqlQuery = "SELECT * from " + comboBox1.Items[0];
+                        MySqlCommand cmd = new MySqlCommand(sqlQuery, sqlConn);
+                        MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                        DataTable table = new DataTable();
+                        try
+                        {
+                            da.Fill(table);
+                        }
+                        catch (MySqlException exc)
+                        {
+                            MessageBox.Show("No se encuentra la tabla " + comboBox1.Items[0], "Error!");
+                            return;
+                        }
+                        dataGridView1.DataSource = new BindingSource(table, null);
+                        sqlConn.Close();
+                        comboBox1.Text = comboBox1.Items[0].ToString();
+                        comboBox1.Enabled = true;
+                        textBox2.Enabled = true;
+                        label4.Text = comboBox1.Items[0].ToString();
+                        MessageBox.Show("Conectado con " + textBox1.Text, "Correcto!");
+                    }
+                    catch (MySqlException exc)
+                    {
+                        MessageBox.Show("Error al conectar con " + textBox1.Text, "Error!");
+                    }
+                }
+            }
+        }
+
+        public List<string> MySqlCollectionQuery(MySqlConnection connection, string cmd)
+        {
+            List<string> QueryResult = new List<string>();
+            MySqlCommand cmdName = new MySqlCommand(cmd, connection);
+            MySqlDataReader reader = cmdName.ExecuteReader();
+            while (reader.Read())
+            {
+                QueryResult.Add(reader.GetString(0));
+            }
+            reader.Close();
+            return QueryResult;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            using (sqlConn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    sqlConn.Open();
+                    string sqlQuery = "SELECT * from " + comboBox1.Text;
+                    MySqlCommand cmd = new MySqlCommand(sqlQuery, sqlConn);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    try
+                    {
+                        da.Fill(table);
+                    }
+                    catch (MySqlException exc)
+                    {
+                        MessageBox.Show("No se encuentra la tabla " + comboBox1.Text, "Error!");
+                        return;
+                    }
+                    dataGridView1.DataSource = new BindingSource(table, null);
+                    label4.Text = comboBox1.Text;
+                    sqlConn.Close();
+                }
+                catch (MySqlException exc)
+                {
+                    MessageBox.Show("Ha ocurrido un problema con la conexión", "Error!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConn.Open();
+                string sqlQuery = textBox2.Text;
+                if (sqlQuery.Split(' ')[0].ToUpper().Equals("SELECT"))
+                {
+                    MySqlCommand cmd = new MySqlCommand(sqlQuery, sqlConn);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    try
+                    {
+                        da.Fill(table);
+                    }
+                    catch (MySqlException exc)
+                    {
+                        MessageBox.Show("No se ha podido ejecutar su query", "Error!");
+                        return;
+                    }
+                    dataGridView1.DataSource = new BindingSource(table, null);
+                    label4.Text = comboBox1.Text;
+                }
+                sqlConn.Close();
+            }
+            catch (MySqlException exc)
+            {
+                MessageBox.Show("Ha ocurrido un problema con la conexión", "Error!");
+            }
+        }
+    }
+}
